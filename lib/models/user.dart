ac516524c3ac566dc566716424de9fef97cf9d87
abdulrahman-kharzoum <aboodkh920@gmail.com>
2023-05-19 22:17:34 +0300

    init

---
+++ lib/models/user.dart
@@ -0,0 +1,6 @@
+class User {
+  String userName;
+  String password;
+  int? phoneNumber;
+  User({required this.userName, required this.password, this.phoneNumber});
+}
---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+
+class AppColors {
+  static Color primaryColor = Colors.black;
+  static Color secondaryColor = Colors.blue;
+  static Color appColor = Colors.green;
+  static Color iconColor = Colors.purple;
+}
