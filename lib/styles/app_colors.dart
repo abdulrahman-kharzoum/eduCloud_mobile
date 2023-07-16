215c1d65fb42f98baa6161789a4b9c7ac37412eb
Alaa-shibany <kaibablack47@gmail.com>
2023-07-16 21:04:49 +0300

    home screen & drawer with translate

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -1,8 +1,17 @@
 import "package:flutter/material.dart";
 
 class AppColors {
-  static Color primaryColor = Colors.black;
-  static Color secondaryColor = Colors.blue;
-  static Color appColor = Colors.green;
-  static Color iconColor = Colors.purple;
+  static Color primaryColor = const Color.fromRGBO(61, 61, 110, 1);
+  static Color secondaryColor = Colors.white;
+  static Color appColor = Colors.deepPurpleAccent.shade400;
+  static Color iconColor = Colors.white;
+  static Color titleTextColor = const Color.fromRGBO(55, 53, 89, 1);
+  static Color textColor = const Color.fromRGBO(150, 150, 150, 1);
+  static Color profileColor = const Color.fromRGBO(44, 81, 139, 1);
+  static Color lightBlue = const Color.fromRGBO(173, 230, 254, 1);
+  static Color mainColor = const Color.fromRGBO(34, 62, 109, 1);
+  static List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
 }
--- lib/styles/app_text_styles.dart
+++ lib/styles/app_text_styles.dart
@@ -1,1 +1,30 @@
-class AppTextStyles {}
+import 'package:educloud_mobile/styles/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle textTitleStyle = TextStyle(
+      color: AppColors.titleTextColor,
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Euclid Circular A');
+  static TextStyle textSubTitleStyle = TextStyle(
+      color: AppColors.textColor,
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      fontFamily: 'Euclid Circular A');
+  static TextStyle profile = TextStyle(
+      color: AppColors.titleTextColor,
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Euclid Circular A');
+  static TextStyle profile2 = TextStyle(
+      color: AppColors.textColor,
+      fontSize: 15,
+      fontWeight: FontWeight.normal,
+      fontFamily: 'Euclid Circular A');
+  static TextStyle miniTitle = TextStyle(
+      color: AppColors.titleTextColor,
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+      fontFamily: 'Euclid Circular A');
+}
