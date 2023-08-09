cdfe827231d6052c3f2c11618675ab1f90b47271
abdulrahman-kharzoum <aboodkh920@gmail.com>
2023-08-09 21:02:03 +0300

    clean some code

--- lib/screens/live_location.dart
+++ lib/screens/live_location.dart
@@ -35,64 +35,6 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
         .then((controller) => controller.dispose()); // Dispose of controller
     super.dispose();
   }
-  // bool _loading = false;
-  // int _polylineCount = 1;
-  // Map<PolylineId, Polyline> _polylines = <PolylineId, Polyline>{};
-  // GoogleMapPolyline _googleMapPolyline =
-  //     new GoogleMapPolyline(apiKey: google_api_key);
-
-  // //Polyline patterns
-  // List<List<PatternItem>> patterns = <List<PatternItem>>[
-  //   <PatternItem>[], //line
-  //   <PatternItem>[PatternItem.dash(30.0), PatternItem.gap(20.0)], //dash
-  //   <PatternItem>[PatternItem.dot, PatternItem.gap(10.0)], //dot
-  //   <PatternItem>[
-  //     //dash-dot
-  //     PatternItem.dash(30.0),
-  //     PatternItem.gap(20.0),
-  //     PatternItem.dot,
-  //     PatternItem.gap(20.0)
-  //   ],
-  // ];
-  // //Get polyline with Location (latitude and longitude)
-  // _getPolylinesWithLocation() async {
-  //   _setLoadingMenu(true);
-  //   List<LatLng>? _coordinates =
-  //       await _googleMapPolyline.getCoordinatesWithLocation(
-  //           origin: sourceLocation,
-  //           destination: destination,
-  //           mode: RouteMode.driving);
-
-  //   setState(() {
-  //     _polylines.clear();
-  //   });
-  //   _addPolyline(_coordinates);
-  //   _setLoadingMenu(false);
-  // }
-
-  // //Get polyline with Address
-
-  // _addPolyline(List<LatLng>? _coordinates) {
-  //   PolylineId id = PolylineId("poly$_polylineCount");
-  //   Polyline polyline = Polyline(
-  //       polylineId: id,
-  //       patterns: patterns[0],
-  //       color: Colors.blueAccent,
-  //       points: _coordinates!,
-  //       width: 10,
-  //       onTap: () {});
-
-  //   setState(() {
-  //     _polylines[id] = polyline;
-  //     _polylineCount++;
-  //   });
-  // }
-
-  // _setLoadingMenu(bool _status) {
-  //   setState(() {
-  //     _loading = _status;
-  //   });
-  // }
 
   void setCustomMarkerIcon() async {
     final Uint8List markerSourceIcon =
@@ -104,57 +46,10 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
     sourceIcon = BitmapDescriptor.fromBytes(markerSourceIcon);
     destinationIcon = BitmapDescriptor.fromBytes(markerDestinationIcon);
     currentLocationIcon = BitmapDescriptor.fromBytes(markerCurrentLocationIcon);
-    // BitmapDescriptor.fromAssetImage(
-    //         ImageConfiguration.empty, "assets/location/university.png")
-    //     .then(
-    //   (icon) {
-    //     sourceIcon = icon;
-    //   },
-    // );
-    // BitmapDescriptor.fromAssetImage(
-    //         ImageConfiguration.empty, "assets/location/bus-station.png")
-    //     .then(
-    //   (icon) {
-    //     destinationIcon = icon;
-    //   },
-    // );
-    // BitmapDescriptor.fromAssetImage(
-    //         ImageConfiguration.empty, "assets/location/bus.png")
-    //     .then(
-    //   (icon) {
-    //     currentLocationIcon = icon;
-    //   },
-    // );
   }
 
   LocationData? currentLocation;
-  // void getCurrentLocation() async {
-  //   Location location = Location();
-  //   location.getLocation().then(
-  //     (location) {
-  //       currentLocation = location;
-  //       setState(() {});
-  //     },
-  //   );
-  //   GoogleMapController googleMapController = await _controller.future;
-  //   location.onLocationChanged.listen(
-  //     (newLoc) {
-  //       currentLocation = newLoc;
-  //       googleMapController.animateCamera(
-  //         CameraUpdate.newCameraPosition(
-  //           CameraPosition(
-  //             zoom: 13.5,
-  //             target: LatLng(
-  //               newLoc.latitude!,
-  //               newLoc.longitude!,
-  //             ),
-  //           ),
-  //         ),
-  //       );
-  //       setState(() {});
-  //     },
-  //   );
-  // }
+
   void getCurrentLocation() async {
     Location location = Location();
     locationSubscription = location.onLocationChanged.listen(
@@ -184,23 +79,6 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
     );
   }
 
-  // List<LatLng> polylineCoordinates = [];
-  // void getPolyPoints() async {
-  //   PolylinePoints polylinePoints = PolylinePoints();
-  //   PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-  //     google_api_key, // Your Google Map Key
-  //     PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
-  //     PointLatLng(destination.latitude, destination.longitude),
-  //   );
-  //   if (result.points.isNotEmpty) {
-  //     result.points.forEach(
-  //       (PointLatLng point) => polylineCoordinates.add(
-  //         LatLng(point.latitude, point.longitude),
-  //       ),
-  //     );
-  //     setState(() {});
-  //   }
-  // }
   Future<Uint8List> getBytesFromAsset(String path, int width) async {
     ByteData data = await rootBundle.load(path);
     ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
@@ -246,15 +124,6 @@ class OrderTrackingPageState extends State<OrderTrackingPage> {
               onMapCreated: (mapController) {
                 _controller.complete(mapController);
               },
-              //  polylines: Set<Polyline>.of(_polylines.values),
-              // polylines: {
-              //   Polyline(
-              //     polylineId: const PolylineId("route"),
-              //     points: polylineCoordinates,
-              //     color: const Color(0xFF7B61FF),
-              //     width: 6,
-              //   ),
-              // },
             ),
     );
   }
